359f86d81645d74bf0463b66af3b143d40891a3e
giovannaarrais <giovannaarrais13@gmail.com>
2025-02-01 21:28:23 -0300

    :sparkles: feat: aprimorar layout de login e adicionar variáveis de estilo

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    DefaultLoginLayoutComponent
+    DefaultLoginLayoutComponent, ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  loginForm!: FormGroup;
+
+  constructor() {
+
+  }
 
 }
