985121dcf634cdcbf4d26184cf7e6183efc3a811
giovannaarrais <giovannaarrais13@gmail.com>
2025-02-04 21:05:22 -0300

    :bug: ERROR RuntimeError: NG01050: implementar ControlValueAccessor no componente de entrada primária e ajustar a integração com o formulário de login

--- src/app/components/primary-input/primary-input.component.ts
+++ src/app/components/primary-input/primary-input.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, input } from '@angular/core';
-import { ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { Component, forwardRef, Input, input } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 type InputTypes = "text" | "email" | "password";
 
@@ -7,13 +7,48 @@ type InputTypes = "text" | "email" | "password";
   selector: 'app-primary-input',
   standalone: true,
   imports: [ReactiveFormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PrimaryInputComponent),
+      multi: true,
+    }
+  ],
   templateUrl: './primary-input.component.html',
   styleUrl: './primary-input.component.scss'
 })
-export class PrimaryInputComponent {
-  @Input() formGroup!: FormGroup;
+export class PrimaryInputComponent implements ControlValueAccessor{
   @Input() type: InputTypes = "text";
-  @Input() formName: string = "";
   @Input() placeholder: string = "";
   @Input() label: string = "";
+  @Input() inputName: string = "";
+
+  //metodos
+  value: string = "";
+
+  //functions
+  onChange: any = () => {};
+  OnTouched: any = () => {};
+
+  //quando input receber um valor
+  onInput(event: Event){
+    const value = (event.target as HTMLInputElement).value
+    this.onChange(value)
+  }
+
+  writeValue(value: any): void {
+      this.value = value
+  }
+
+  registerOnChange(fn: any): void {
+      this.onChange = fn
+  }
+
+  registerOnTouched(fn: any): void {
+      this.OnTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {}
+
+
 }
