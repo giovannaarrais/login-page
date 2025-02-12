8d6931948e1d473553fc87098c7bdc7c2b2ec1e3
giovannaarrais <giovannaarrais13@gmail.com>
2025-02-11 22:51:10 -0300

    :sparkles: feat: adicionar componente de signup com validação de formulário e navegação para login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { SignupComponent } from './page/signup/signup.component';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './page/login/login.component';
 
@@ -5,5 +6,9 @@ export const routes: Routes = [
     {
         path: "login",
         component: LoginComponent,
+    },
+    {
+        path: "signup",
+        component: SignupComponent,
     }
 ];
